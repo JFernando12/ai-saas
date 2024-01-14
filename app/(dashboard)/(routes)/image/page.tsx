2f00d729b7174e11914848b7a5b13f2d77e21472
Fernando Castrejon <fernandocastrejonh@gmail.com>
2024-01-14 14:37:02 -0600

    changes

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import axios from 'axios';
-import { ImageIcon, MessageSquare } from 'lucide-react';
+import { Download, ImageIcon, MessageSquare } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { Heading } from '@/components/heading';
-import { formSchema } from './constanst';
+import { amountOptions, formSchema, resolutionsOptions } from './constanst';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
@@ -18,6 +18,9 @@ import { Loader } from '@/components/loader';
 import { UserAvatar } from '@/components/user-avatar';
 import { BotAvatar } from '@/components/bot-avatar';
 import { cn } from '@/lib/utils';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Card, CardFooter } from '@/components/ui/card';
+import Image from 'next/image';
 
 const ImagePage = () => {
   const route = useRouter();
@@ -27,6 +30,8 @@ const ImagePage = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       prompt: '',
+      amount: '1',
+      resolution: '512x512'
     },
   });
 
@@ -34,8 +39,12 @@ const ImagePage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const response = await axios.post('/api/conversation');
+      setImages([]);
+      const response = await axios.post('/api/image', values);
 
+      const urls = response.data.map((image: { url: string}) => (image.url));
+
+      setImages(urls);
       form.reset()
     } catch (error) {
       console.log(error);
@@ -65,14 +74,74 @@ const ImagePage = () => {
                 disabled={isLoading}
                 name="prompt"
                 render={({ field }) => (
-                  <FormItem className="col-span-12 lg:col-span-10">
+                  <FormItem className="col-span-12 lg:col-span-6">
                     <FormControl className="m-0 p-0">
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-                        placeholder="How do I calculate the radius of a circle?"
+                        placeholder="A picture of a horse in Swiss alps"
                         {...field}
                       />
                     </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='amount'
+                render={({ field }) => (
+                  <FormItem className='col-span-12 lg:col-span-2'>
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue defaultValue={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {amountOptions.map((option) => (
+                          <SelectItem
+                            key={option.value}
+                            value={option.value}
+                          >
+                            {option.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='resolution'
+                render={({ field }) => (
+                  <FormItem className='col-span-12 lg:col-span-2'>
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue defaultValue={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {resolutionsOptions.map((option) => (
+                          <SelectItem
+                            key={option.value}
+                            value={option.value}
+                          >
+                            {option.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
                   </FormItem>
                 )}
               />
@@ -88,15 +157,35 @@ const ImagePage = () => {
         </div>
         <div className="space-y-4 mt-4">
           {isLoading && (
-            <div className="bg-muted p-8 rounded-lg w-full flex items-center justify-center">
+            <div className="p-20">
               <Loader />
             </div>
           )}
           {images.length === 0 && !isLoading && (
             <Empty label="No images generated." />
           )}
-        <div>
-          Image will be here
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8'>
+            {images.map((src) => (
+              <Card key={src} className='rounded-lg overflow-hidden'>
+                <div className='relative aspect-square'>
+                  <Image
+                    alt='Image'
+                    fill
+                    src={src}
+                  />
+                </div>
+                <CardFooter className='p-2'>
+                  <Button
+                    onClick={() => window.open(src)}
+                    variant='secondary'
+                    className='w-full'
+                  >
+                    <Download className='h-4 w-4 mr-2' />
+                    Download
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
         </div>
         </div>
       </div>
